bd6e405cfa88503ad121190a6d639e961fd27ad1
hoangnhan2ka3 <workwith.hnhan@gmail.com>
2026-03-05 15:39:40 +0700

    fix: They fixed tailwind ts config, so remove `turbopack.ignoreIssue`

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ import {
     PHASE_PRODUCTION_BUILD
 } from "next/constants"
 
-const tailwindModuleNotFoundRegex = /^Module not found/i
-
 const nextConfig = (phase: string): NextConfig => {
     const isDev = phase === PHASE_DEVELOPMENT_SERVER
     const isBuild = phase === PHASE_PRODUCTION_BUILD
@@ -28,15 +26,6 @@ const nextConfig = (phase: string): NextConfig => {
         typedRoutes: true,
         devIndicators: {
             position: "bottom-right"
-        },
-        turbopack: {
-            ignoreIssue: [
-                /** @see {@link https://github.com/vercel/next.js/issues/87898} */
-                {
-                    path: "tailwind.config.ts",
-                    title: tailwindModuleNotFoundRegex
-                }
-            ]
         },
         images: { unoptimized: true }
     }
