19736a748a8a4ad2b4aa07faeac40030769d6352
Nguyễn Hoàng Nhân <workwith.hnhan@gmail.com>
2026-08-18 15:32:36 +0700

    fix: TocList direction

--- src/app/portfolio/_components/_layout/toc/toc-list.tsx
+++ src/app/portfolio/_components/_layout/toc/toc-list.tsx
@@ -86,7 +86,7 @@ function TocList({
             className={cn(
                 "group overflow-x-hidden overflow-y-scroll scroll-auto py-3 scrollbar-none",
                 "scroll-fade-y scroll-fade-18",
-                "hover:scrollbar-thin group-data-[sidebar-position=left]/html:ltr",
+                "hover:scrollbar-thin group-data-[sidebar-position=left]/html:rtl",
                 className
             )}
             {...props}
