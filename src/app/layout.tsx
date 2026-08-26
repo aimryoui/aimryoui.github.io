b64ecbdd00dbd1b47ee85a9e076547f69acb3e73
hoangnhan2ka3 <workwith.hnhan@gmail.com>
2026-08-26 16:54:39 +0700

    fix: Turn off debug safe zone spacing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -164,10 +164,10 @@ export default function RootLayout({
                 googleSansFlex.variable,
                 sfMono.variable,
                 "group/html antialiased scrollbar-colored scrollbar-thumb-default/40 scrollbar-track-pattern",
-                "[--safe-area-inset-top:32px]",
-                "[--safe-area-inset-bottom:32px]",
-                "[--safe-area-inset-left:72px]",
-                "[--safe-area-inset-right:72px]"
+                "[--safe-area-inset-top:env(safe-area-inset-top,0px)]",
+                "[--safe-area-inset-bottom:env(safe-area-inset-bottom,0px)]",
+                "[--safe-area-inset-left:env(safe-area-inset-left,0px)]",
+                "[--safe-area-inset-right:env(safe-area-inset-right,0px)]"
             )}
         >
             <head>
