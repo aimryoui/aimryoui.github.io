0d1ef0afd97fe4d52a7d41e6063405734b35a136
hoangnhan2ka3 <workwith.hnhan@gmail.com>
2026-08-27 12:55:48 +0700

    fix: Allow blob: as media-src in CSP fix: Improve CSP

--- src/configs/csp.config.ts
+++ src/configs/csp.config.ts
@@ -3,11 +3,15 @@ const cspDirectives = [
     "script-src 'self' https://www.googletagmanager.com https://www.google-analytics.com",
     "style-src 'self' 'unsafe-inline'",
     "img-src 'self' blob: data: https://www.googletagmanager.com https://www.google-analytics.com",
+    "media-src 'self' blob:",
     "connect-src 'self' https://www.googletagmanager.com https://www.google-analytics.com",
     "font-src 'self'",
+    "frame-src 'none'",
     "object-src 'none'",
     "base-uri 'self'",
     "form-action 'self'",
+    "worker-src 'self'",
+    "manifest-src 'self'",
     "upgrade-insecure-requests"
 ]
 
